3928961ccf50fa4e8036d4f8cb140bcfdaec3ae3
VivianeMW <mwviviane@gmail.com>
2019-10-04 01:05:05 -0300

    tela de login

---
+++ app/src/main/java/com/br/iroom/view/ActivityLogin.java
@@ -0,0 +1,16 @@
+package com.example.teste.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.teste.R;
+
+public class ActivityLogin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
